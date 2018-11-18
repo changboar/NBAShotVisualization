56e42c1b796e42828b96edf6dfb3c97eb537b116
chhdivinity <r99522712@ntu.edu.tw>
2018-11-18 14:49:57 -0800

    add minCount slider bar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import nba from 'nba';
-import { ShotChart } from './ShotChart';
+
 import {Profile} from "./Profile"
+import { DataViewContainer } from './DataViewContainer';
+
+
 
 export class Main extends React.Component {
 
@@ -25,7 +28,7 @@ export class Main extends React.Component {
     return(
       <div className="main">
         <Profile playerId={this.state.playerId} playerInfo={this.state.playerInfo}/>
-        <ShotChart playerId={this.state.playerId}/>
+        <DataViewContainer playerId={this.state.playerId}/>
       </div>
     );
 
